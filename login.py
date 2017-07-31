020ea62f6f45d75c8d7f53b0b86de441b6f249db
SachinHg <sachin0892@gmail.com>
2017-07-31 12:14:03 +0530

    Delete login.py

--- login.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = 'http://172.16.0.30:8090/httpclient.html'
-values = {'username':'h2016034',
-			'password': 'Sh@@rawy92'}
-r = requests.post(url,data=values)
-print (r.content)
